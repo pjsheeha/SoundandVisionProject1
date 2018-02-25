c571919a1ddeac6312716c5158d5958e1fcf567d
pjsheeha@andrew.cmu.edu <pjsheeha@andrew.cmu.edu>
2018-02-24 20:42:47 -0500

    Camera, char, fog

---
+++ SoundandVision/Assets/ParticleCloudRegulator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCloudRegulator : MonoBehaviour {
+
+	public ParticleSystem abc;
+	public int rate = 50;
+
+
+	void Start()
+	{
+		abc = GetComponent<ParticleSystem> ();
+	}
+
+	void Update () {
+		var emission = abc.emission;
+		emission.rateOverTime = rate;
+
+	}
+}
