f618df556eb96d8bac2c590ad28730094a7df956
pjsheeha@andrew.cmu.edu <pjsheeha@andrew.cmu.edu>
2018-03-16 05:02:58 -0400

    Guy walks on hills now (in grass scene) and there is a grass renderer prefab renderer (buggy)

--- SoundandVisionCheckin/Assets/Scripts/General/GrassRenderer.cs
+++ SoundandVisionCheckin/Assets/Scripts/General/GrassRenderer.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour {
-
+[ExecuteInEditMode]
+public class GrassRenderer : MonoBehaviour {
+	public int seed;
+	public Mesh grassMesh;
+	public Material material;
+	public Vector2 size;
+	[Range(1,1000)]
+	public int grassNumber;
+	public float startHeight = 1000;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,20 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Random.InitState (seed);
+		List<Matrix4x4> matrices = new List<Matrix4x4> (grassNumber);
+		for (int i = 0; i < grassNumber; i++) {
+			Vector3 origin = transform.position;
+			origin.y = startHeight;
+			origin.x += size.x * Random.Range (-0.5f,0.5f);
+			origin.z += size.y * Random.Range (-0.5f,0.5f);
+			Ray ray = new Ray (origin, Vector3.down);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				origin = hit.point;
+			}
+			matrices.Add (Matrix4x4.TRS (origin, Quaternion.identity, Vector3.one));
+		}
+		Graphics.DrawMeshInstanced (grassMesh, 0, material, matrices);
 	}
 }
